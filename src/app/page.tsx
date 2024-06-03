5517c5ef6ac5a980f5ff0473db795785a57ca5e0
dharam-in <dharamdotin@gmail.com>
2024-06-03 17:10:45 +0530

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center flex-col min-h-[100vh] mx-auto py-10">
       <div className="flex items-center justify-between mb-6">
         <div className='text-center'>
-          <h1 className="text-4xl font-bold text-primary mb-2">Welcome to "ProElevate's"</h1>
+          <h1 className="text-4xl font-bold text-primary mb-2">Welcome to ProElevate</h1>
           <p className="text-lg text-gray-600 dark:text-gray-300">Where every moment becomes an event.</p>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <Link href="/new-event" className='bg-blue-500 text-white py-4 px-6 rounded-md shadow-md hover:bg-blue-600 block'>
             Create New Event
-
         </Link>
         <Link href="/events" className='bg-green-500 text-white py-4 px-6 rounded-md shadow-md hover:bg-green-600 block' >
             View Events
